327ebc0f26395b25682013fdbb003579027ecd66
Allan Schran <allan@schran.com>
2011-06-18 10:51:06 -0300

    Script SQL alterado

--- script_BD_POO.sql
+++ script_BD_POO.sql
@@ -1,48 +1,48 @@
-CREATE TABLE Cargo (
+CREATE TABLE cargo (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nomeCargo VARCHAR NOT NULL,
+  nome VARCHAR(30) NOT NULL,
   PRIMARY KEY(id)
 );
 
-CREATE TABLE Chamados (
+CREATE TABLE chamados (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   Colaboradores_id INTEGER UNSIGNED NOT NULL,
-  Computadores_id INTEGER UNSIGNED NOT NULL,
+  computadores_id INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY(id),
-  INDEX Chamados_FKIndex4(Computadores_id)
+  INDEX chamados_FKIndex4(computadores_id)
 );
 
-CREATE TABLE Colaboradores (
+CREATE TABLE colaboradores (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  Cargo_id INTEGER UNSIGNED NOT NULL,
-  nomeColaborador VARCHAR NOT NULL,
-  descricaoColaborador VARCHAR NOT NULL,
+  cargo_id INTEGER UNSIGNED NOT NULL,
+  nome VARCHAR(30) NOT NULL,
+  descricao VARCHAR(100) NOT NULL,
   PRIMARY KEY(id),
-  INDEX Colaboradores_FKIndex1(Cargo_id)
+  INDEX colaboradores_FKIndex1(cargo_id)
 );
 
-CREATE TABLE Computadores (
+CREATE TABLE computadores (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  descricaoHardware VARCHAR NOT NULL,
-  decricaoSoftware VARCHAR NOT NULL,
+  descricaoHardware VARCHAR(200) NOT NULL,
+  decricaoSoftware VARCHAR(200) NOT NULL,
   PRIMARY KEY(id)
 );
 
-CREATE TABLE Estados (
+CREATE TABLE estados (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome VARCHAR NOT NULL,
+  nome VARCHAR(50) NOT NULL,
   PRIMARY KEY(id)
 );
 
-CREATE TABLE Mensagens (
+CREATE TABLE mensagens (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  Chamados_id INTEGER UNSIGNED NOT NULL,
-  Estados_id INTEGER UNSIGNED NOT NULL,
-  conteudoMensagem VARCHAR NOT NULL,
-  horarioMensagem DATE NOT NULL,
+  chamados_id INTEGER UNSIGNED NOT NULL,
+  estados_id INTEGER UNSIGNED NOT NULL,
+  conteudo VARCHAR(500) NOT NULL,
+  horario DATE NOT NULL,
   PRIMARY KEY(id),
-  INDEX Mensagns_FKIndex1(Estados_id),
-  INDEX Mensagns_FKIndex2(Chamados_id)
+  INDEX mensagens_FKIndex1(estados_id),
+  INDEX mensagens_FKIndex2(Chamados_id)
 );
 
 
